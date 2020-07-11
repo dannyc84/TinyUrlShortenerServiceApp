e4d3cbb806b86bd4cd01644bb46a2a5b1ee55ddd
daniele.crivello <daniele.crivello84@libero.it>
2020-07-11 19:04:51 +0200

    Moved the console error message when conversion is failing to the right place

--- TinyUrlShortenerServiceApp/TinyUrlShortenerApp.cs
+++ TinyUrlShortenerServiceApp/TinyUrlShortenerApp.cs
@@ -20,15 +20,14 @@ namespace TinyUrlShortenerServiceApp
             else if (args.Length != 1)
                 throw new ArgumentException("The number of arguments must be only one");
 
-            if (int.TryParse(args[0], out int urlId))
-            {
-                var encodedUrl = _urlShortenerService.Encode(urlId);
-                Console.WriteLine($"Encoded URL: {encodedUrl}");
-                var decodedUrl = _urlShortenerService.Decode(encodedUrl);
-                Console.WriteLine($"Decoded URL : {decodedUrl}");
-            }
-            Console.WriteLine("Attempted conversion of '{0}' failed.",
+            if (!int.TryParse(args[0], out int urlId))
+                Console.WriteLine("Attempted conversion of '{0}' failed.",
                                args[0] ?? "<null>");
+            var encodedUrl = _urlShortenerService.Encode(urlId);
+            Console.WriteLine($"Encoded URL: {encodedUrl}");
+            var decodedUrl = _urlShortenerService.Decode(encodedUrl);
+            Console.WriteLine($"Decoded URL : {decodedUrl}");
+
         }
     }
 }
